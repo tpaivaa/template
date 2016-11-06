aabb1f1487f07d15f8407e5ee8554a4a89d0c8c3
tpaivaa <tpaivaa@gmail.com>
2016-11-06 21:54:12 +0200

    First working version for tests

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,27 @@
-angular.module('wallboard', [])
-      .controller('wallCtrl', ['$scope', function ($scope) {
-        $scope.name = 'Hello World';
-      }]);
+var wallboard = angular.module('wallboard', [])
+wallboard.controller('wallCtrl', ['$scope','$http','$timeout', function ($scope,$http,$timeout) {
+	$scope.details = {};
+	$scope.details.connected = 'Hello World';
+	$scope.details.routed = 'Hello World';
+	$scope.details.unawailable = 'Hello World';
+	$scope.name = 'Nimi';
+
+	// Function to get the data
+	$scope.getData = function (){
+		$http.get("http://localhost:3000/json")
+		.then(function(response){ $scope.details = response.data; });
+	}
+
+	// Function to replicate setInterval using $timeout service.
+	$scope.intervalFunction = function(){
+		$timeout(function() {
+			$scope.getData();
+			$scope.intervalFunction();
+		}, 1000)
+	};
+
+	// Kick off the interval
+	$scope.intervalFunction();
+
+}]);
+
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var koa = require('koa');
-var app = koa();
+var json = require('koa-json');
 var root = 'public';
+var router = require('koa-router')();
+var serve = require('koa-static');
 var opts = '';
-app.use(require('koa-static')(root, opts));
+var app = koa();
+
+app.use(json());
+app.use(serve(__dirname + '/public'));
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+router.get('/', serve(root,opts));
+
+router.get('/json', function *(next) {
+	this.body = { connected: 'OK', routed: 15, unawailable: 120};
+});
 
 app.listen(3000);
 
 console.log('listening on port 3000');
+
